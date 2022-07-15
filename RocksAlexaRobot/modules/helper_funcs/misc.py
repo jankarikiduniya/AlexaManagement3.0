e0ca83103ec7ade73746511dcf485e1e9fde9c94
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-07-15 10:21:40 +0500

    Pairs Added

--- RocksAlexaRobot/modules/helper_funcs/misc.py
+++ RocksAlexaRobot/modules/helper_funcs/misc.py
@@ -56,29 +56,32 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                                     callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
              in module_dict.values()])
 
-    pairs = [
-    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
-    ]
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
-    if calc == 1:
-        pairs.append((modules[-1], ))
-    elif calc == 2:
-        pairs.append((modules[-1], ))
+    if calc in [1, 2]:
+        pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 100)
+    max_num_pages = ceil(len(pairs) / 14)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 100:
-        pairs = pairs[modulo_page * 100:100 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back to Home", callback_data="alexa_back"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 14 : 14 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "☜", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton("✘ ʙᴀᴄᴋ ✘", callback_data="alexa_back"),
+                EqInlineKeyboardButton(
+                    "☞", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back to Home", callback_data="alexa_back")]]
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="alexa_back")]]
 
     return pairs
 
